75e425fdcb4601c93b30c1bea96c8c1785a52ff2
rashmibk23 <122861886+rashmibk23@users.noreply.github.com>
2023-01-17 21:47:45 +0530

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      echo "<h2>Website</h2>";
+      echo("Hello World<br>");
+
+    //   Variables
+      $characterName = "John";
+      $characterAge = 30;
+      echo "There once was a man named, $characterName <br>";
+      echo "He was $characterAge years old <br>";
+      echo "He really liked the name $characterName <br>";
+      echo "But didn't like being $characterAge <br>";
+
+    //   Data Types
+      $message = "This is a page";
+      $age = 30;
+      $marks = 20.5;
+      $status = true;
+      echo $message;
+      echo $marks;
+    ?>
+</body>
+</html>
